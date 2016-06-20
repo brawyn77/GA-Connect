4556a0171880003c229e9041617c1062b2c6ce4e
lukephelan <luke.phelan@hotmail.com>
2016-06-20 14:13:39 +1000

    Fixed conflict in user.js

---
+++ config.js
@@ -0,0 +1,7 @@
+module.exports = {
+  'secret': 'hellohappypeople',
+  'database': 'mongodb://localhost/test'
+};
+
+// Not sure if this is necessary, or at least, the database is not necessary
+// since we are hosting on MongoDB
--- server.js
+++ server.js
@@ -7,6 +7,11 @@
 var express = require('express');        // call express
 var app = express();                 // define our app using express
 var bodyParser = require('body-parser');
+var morgan = require('morgan');
+
+var jwt = require('jsonwebtoken'); // use to create, sign, and verify tokens
+var config = require('./config'); // get our config file
+
 
 // configure app to use bodyParser()
 // this will let us get the data from a POST
@@ -15,27 +20,114 @@ app.use(bodyParser.json());
 
 var port = process.env.PORT || 8080;        // set our port
 
-var User = require('./models/user');
+// use morgan to log requests to the console
+app.use(morgan('dev'));
+
+var User = require('./models/user'); //  get our Mongoose model
 
 var mongoose = require('mongoose');
 mongoose.connect('mongodb://localhost/test'); // connect to our databa
+app.set('superSecret', config.secret); // secret variable
 // ROUTES FOR OUR API
 // =============================================================================
 var router = express.Router();              // get an instance of the express Router
 
+// route to authenticate a user (POST http://locahost:8080/api/authenticate)
+router.post('/authenticate', function(req, res) {
+
+  // find the user
+  User.findOne({
+    name: req.body.name
+  }, function(err, user) {
+
+    if (err) throw err;
+
+    if (!user) {
+      res.json({success: false, message: 'Authentication failed. User not found.' });
+    } else if (user) {
+
+      // check if password matches
+      if (user.password != req.body.pasword) {
+        res.json({success: false, message: 'Authentication failed. Password is incorrect.' });
+      } else {
+
+        // if user is found and password is correct
+        // create a token
+        var token = jwt.sign(user, app.get('superSecret'), {
+          expiresIn: '1440m' // expires in 24 hours
+        });
+
+        // return the information token as JSON
+        res.json({
+          success: true,
+          message: 'Enjoy your token!',
+          token: token
+        });
+      }
+    }
+  });
+});
+
 // middleware to use for all requests
 router.use(function(req, res, next) {
     // do logging
-    console.log('Testing middleware.');
-    next(); // make sure we go to the next routes and don't stop here
+    // console.log('Testing middleware.');
+    // next(); // make sure we go to the next routes and don't stop here
+    var token = req.body.token || req.query.token || req.headers['x-access-token'];
+
+    // decode token
+    if (token) {
+
+      // verifies secret and checks exp
+      jwt.verify(token, app.get('superSecret'), function(err, decoded) {
+        if (err) {
+          return res.json({success: false, message: 'Failed to authenticate token.' });
+        } else {
+          // if everything is good, save to request for use in other routes
+          req.decoded = decoded;
+          next(); // make sure we go to the next routes and don't stop here
+        }
+      });
+
+    } else {
+
+      // if there is no token
+      // return an error
+      return res.status(403).send({
+        success: false,
+        message: 'No token provided.'
+      });
+    }
 });
+
 
 // test route to make sure everything is working (accessed at GET http://localhost:8080/api)
 router.get('/', function(req, res) {
-    res.json({ message: 'api is working!' });
+    // res.json({ message: 'api is working!' });
+    res.send('Hello! The API is at http://localhost:' + port + '/api');
 });
 
 // more routes for our API will happen here
+
+router.get('/setup', function(req, res) {
+
+  // create a sample user
+  var nick = new User({
+    name: 'Bill',
+    // This is obviously not the way to store passwords
+    password: 'password'
+  });
+
+  // save the sample user
+  nick.save(function(err) {
+    if (err) throw err;
+
+    console.log('User saved successfully');
+    res.json({success: true});
+  });
+});
+
+
 
 // on routes that end in /users
 // ----------------------------------------------------
@@ -96,7 +188,7 @@ router.route('/users')
 	                 if (err)
 	                     res.send(err);
 
-	                 res.json({ message: 'er updated!' });
+	                 res.json({ message: 'err updated!' });
 	             });
 
 	         });
@@ -113,6 +205,8 @@ router.route('/users')
 		           res.json({ message: 'Successfully deleted' });
 		       });
 		   });
+
+
 
 // REGISTER OUR ROUTES -------------------------------
 // all of our routes will be prefixed with /api
