a700a50ffbaf07e5de435f729a90e69181254fc1
lukephelan <luke.phelan@hotmail.com>
2016-06-21 10:27:13 +1000

    Setup passport for logins and registration. Changed how web tokens are generated.

--- models/user.js
+++ models/user.js
@@ -1,4 +1,6 @@
 var mongoose     = require('mongoose');
+var crypto       = require('crypto');
+var jwt          = require('jsonwebtoken');
 var Schema       = mongoose.Schema;
 
 var UserSchema   = new Schema({
@@ -6,7 +8,30 @@ var UserSchema   = new Schema({
     lastName: { type: String, uppercase: true },
     email: { type: String, required: true, lowercase: true, unique: true },
     password: { type: String, required: true },
-    admin: Boolean
+    admin: Boolean,
+    hash: String,
+    salt: String
 });
+
+UserSchema.methods.setPassword = function(password){
+  this.salt = crypto.randomBytes(16).toString('hex');
+  this.hash = crypto.pbkdf2Sync(password, this.salt, 1000, 64).toString('hex');
+};
+
+UserSchema.methods.validPassword = function(password) {
+  var hash = crypto.pbkdf2Sync(password, this.salt, 1000, 64).toString('hex');
+  return this.hash === hash;
+};
+
+UserSchema.methods.generateJwt = function() {
+  var expiry = new Date();
+  expiry.setDate(expiry.getDate() + 7);
+
+  return jwt.sign({
+    _id: this._id,
+    email: this.email,
+    exp: parseInt(expiry.getTime() / 1000),
+  }, "MY_SECRET"); // THIS SHOULD NOT BE KEPT IN THE CODE
+};
 
 module.exports = mongoose.model('User', UserSchema);
--- server.js
+++ server.js
@@ -8,12 +8,18 @@ var express = require('express');        // call express
 var app = express();                 // define our app using express
 var bodyParser = require('body-parser');
 var morgan = require('morgan');
-
+// var path = require('path'); // Not sure if we need this yet
 var jwt = require('jsonwebtoken'); // use to create, sign, and verify tokens
 var config = require('./config'); // get our config file
+var passport = require('passport');
+// var cookieParser = require('cookie-parser'); // Not sure if we need this here or on front end
+var jwt = require('express-jwt');
+var auth = jwt({
+  secret: 'MY_SECRET',
+  userProperty: 'payload' // Don't keep this here, store as env variable
+});
 
-var bcrypt = require('bcrypt');
-const saltRounds = 10;
+
 
 
 // configure app to use bodyParser()
@@ -27,81 +33,93 @@ var port = process.env.PORT || 8080;        // set our port
 app.use(morgan('dev'));
 
 var User = require('./models/user'); //  get our Mongoose model
+require('./config/passport');
 
 var mongoose = require('mongoose');
-mongoose.connect('mongodb://localhost/test'); // connect to our databa
-app.set('superSecret', config.secret); // secret variable
+mongoose.connect('mongodb://localhost/test'); // connect to our database
+// app.set('superSecret', config.secret); // secret variable, might be redundant
+
+// Allow us to set authorisation requirements to routes
+var ctrlProfile = require('./controllers/profile');
+var ctrlAuth = require('./controllers/authentication');
+
 // ROUTES FOR OUR API
 // =============================================================================
 var router = express.Router();              // get an instance of the express Router
 
 // route to authenticate a user (POST http://locahost:8080/api/authenticate)
-router.post('/authenticate', function(req, res) {
-
-  // find the user
-  User.findOne({
-    email: req.body.email
-  }, function(err, user) {
-
-    if (err) throw err;
-
-    if (!user) {
-      res.json({success: false, message: 'Authentication failed. Email not found.' });
-    } else if (user) {
-
-      // check if password matches
-      if (user.password != req.body.password) {
-        res.json({success: false, message: 'Authentication failed. Password is incorrect.' });
-      } else {
-
-        // if user is found and password is correct
-        // create a token
-        var token = jwt.sign(user, app.get('superSecret'), {
-          expiresIn: '1440m' // expires in 24 hours
-        });
-
-        // return the information token as JSON
-        res.json({
-          success: true,
-          message: 'Enjoy your token!',
-          token: token
-        });
-      }
-    }
-  });
-});
+// router.post('/authenticate', function(req, res) {
+//
+//   // find the user
+//   User.findOne({
+//     email: req.body.email
+//   }, function(err, user) {
+//
+//     if (err) throw err;
+//
+//     if (!user) {
+//       res.json({success: false, message: 'Authentication failed. Email not found.' });
+//     } else if (user) {
+//
+//       // check if password matches
+//       if (user.password != req.body.password) {
+//         res.json({success: false, message: 'Authentication failed. Password is incorrect.' });
+//       } else {
+//
+//         // if user is found and password is correct
+//         // create a token
+//         var token = jwt.sign(user, app.get('superSecret'), {
+//           expiresIn: '1440m' // expires in 24 hours
+//         });
+//
+//         // return the information token as JSON
+//         res.json({
+//           success: true,
+//           message: 'Enjoy your token!',
+//           token: token
+//         });
+//       }
+//     }
+//   });
+// });
 
 // middleware to use for all requests
-router.use(function(req, res, next) {
-    // do logging
-    // console.log('Testing middleware.');
-    // next(); // make sure we go to the next routes and don't stop here
-    var token = req.body.token || req.query.token || req.headers['x-access-token'];
+// router.use(function(req, res, next) {
+//     // do logging
+//     // console.log('Testing middleware.');
+//     // next(); // make sure we go to the next routes and don't stop here
 
-    // decode token
-    if (token) {
 
-      // verifies secret and checks exp
-      jwt.verify(token, app.get('superSecret'), function(err, decoded) {
-        if (err) {
-          return res.json({success: false, message: 'Failed to authenticate token.' });
-        } else {
-          // if everything is good, save to request for use in other routes
-          req.decoded = decoded;
-          next(); // make sure we go to the next routes and don't stop here
-        }
-      });
+    // ALL THIS CAN PROBABLY BE DELETED DOWN TO THE #####
+//     var token = req.body.token || req.query.token || req.headers['x-access-token'];
+//
+//     // decode token
+//     if (token) {
+//
+//       // verifies secret and checks exp
+//       jwt.verify(token, app.get('superSecret'), function(err, decoded) {
+//         if (err) {
+//           return res.json({success: false, message: 'Failed to authenticate token.' });
+//         } else {
+//           // if everything is good, save to request for use in other routes
+//           req.decoded = decoded;
+//           next(); // make sure we go to the next routes and don't stop here
+//         }
+//       });
+//
+//     } else {
+//
+//       // if there is no token
+//       // return an error
+//       return res.status(403).send({
+//         success: false,
+//         message: 'No token provided.'
+//       });
+//     }
 
-    } else {
 
-      // if there is no token
-      // return an error
-      return res.status(403).send({
-        success: false,
-        message: 'No token provided.'
-      });
-    }
-});
+      // #############################
+// });
 
 
 // test route to make sure everything is working (accessed at GET http://localhost:8080/api)
@@ -112,27 +130,34 @@ router.get('/', function(req, res) {
 
 // more routes for our API will happen here
 
-router.get('/setup', function(req, res) {
+// router.get('/setup', function(req, res) {
+//
+//   // create a sample user
+//   var nick = new User({
+//     firstName: 'Nick',
+//     lastName: 'Butcher',
+//     email: 'nick@example.com',
+//     // This is obviously not the way to store passwords
+//     password: 'password',
+//     admin: false
+//   });
+//
+//   // save the sample user
+//   nick.save(function(err) {
+//     if (err) throw err;
+//
+//     console.log('User saved successfully');
+//     res.json({success: true});
+//   });
+// });
 
-  // create a sample user
-  var nick = new User({
-    firstName: 'Nick',
-    lastName: 'Butcher',
-    email: 'nick@example.com',
-    // This is obviously not the way to store passwords
-    password: 'password',
-    admin: false
-  });
 
-  // save the sample user
-  nick.save(function(err) {
-    if (err) throw err;
+// profile
+router.get('/profile', auth, ctrlProfile.profileRead);
 
-    console.log('User saved successfully');
-    res.json({success: true});
-  });
-});
-
+// authentication
+router.post('/register', ctrlAuth.register);
+router.post('/login', ctrlAuth.login);
 
 
 // on routes that end in /users
@@ -140,33 +165,33 @@ router.get('/setup', function(req, res) {
 router.route('/users')
 
     // create a user (accessed at POST http://localhost:8080/api/users)
-    .post(function(req, res) {
-
-        var user = new User();      // create a new instance of the User model
-        user.firstName = req.body.firstName;  // set the users name (comes from the request)
-        user.lastName = req.body.lastName;
-        user.email = req.body.email;
-        // Need bcrypt to hash password
-        const password = req.body.password;
-        var hash = bcrypt.hashSync(password, saltRounds);
-        user.password = hash;
-        user.admin = req.body.admin; // Might need to be adjusted since this is boolean
-
-        // save the user and check for errors
-        user.save(function(err) {
-            if (err) {
-              res.send(err);
-            } else {
-
-            res.json({ message: 'User created!' });
-        };
-      });
-      console.log(user);
-
-    })
+    // .post(function(req, res) {
+    //
+    //     var user = new User();      // create a new instance of the User model
+    //     user.firstName = req.body.firstName;  // set the users name (comes from the request)
+    //     user.lastName = req.body.lastName;
+    //     user.email = req.body.email;
+    //     // Need bcrypt to hash password
+    //     const password = req.body.password;
+    //     var hash = bcrypt.hashSync(password, saltRounds);
+    //     user.password = hash;
+    //     user.admin = req.body.admin; // Might need to be adjusted since this is boolean
+    //
+    //     // save the user and check for errors
+    //     user.save(function(err) {
+    //         if (err) {
+    //           res.send(err);
+    //         } else {
+    //
+    //         res.json({ message: 'User created!' });
+    //     };
+    //   });
+    //   console.log(user);
+    //
+    // })
 
 		// get all the users (accessed at GET http://localhost:8080/api/users)
-		.get(function(req, res) {
+		.get(auth, ctrlProfile.profileRead, function(req, res) {
 		    User.find(function(err, users) {
 		      if (err)
 		        res.send(err);
@@ -180,7 +205,7 @@ router.route('/users')
 		router.route('/users/:user_id')
 
 		// get the user with that id (accessed at GET http://localhost:8080/api/users/:user_id)
-		.get(function(req, res) {
+		.get(auth, ctrlProfile.profileRead, function(req, res) {
 		    User.findById(req.params.user_id, function(err, user) {
 		        if (err)
 		            res.send(err);
@@ -217,7 +242,7 @@ router.route('/users')
 	     })
 
 			 // delete the user with this id (accessed at DELETE http://localhost:8080/api/users/:user_id)
-			 .delete(function(req, res) {
+			 .delete(auth, ctrlProfile.profileRead, function(req, res) {
 			     User.remove({
 			         _id: req.params.user_id
 			     }, function(err, user) {
@@ -232,6 +257,7 @@ router.route('/users')
 
 // REGISTER OUR ROUTES -------------------------------
 // all of our routes will be prefixed with /api
+app.use(passport.initialize());
 app.use('/api', router);
 
 // START THE SERVER
